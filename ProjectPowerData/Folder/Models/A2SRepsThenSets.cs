06336ea1d4a5ce5b53055b209d9e32a2d128e3a8
FIRST_NAME LAST_NAMEgit config --global user.name FIRST_NAME <joeyrichardson96@gmail.com>
2021-12-04 13:06:16 +0000

    * A2SRepsThenSets.cs: * ExerciseDays.cs: * A2SDailyWorkoutModel.cs: * UnassignedWorkoutModel.cs: * ShowBasicWorkoutInformationModel.cs: * BasicWorkoutInformationSearchModel.cs: * CreateBasicWorkoutInformationModel.cs: * UpdateBasicWorkoutInformationModel.cs:
    * ProjectPower.sln: Refactored to Workout Management Controller/
      Service

---
+++ ProjectPowerData/Folder/Models/A2SRepsThenSets.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectPowerData.Folder.Models
+{
+    public class A2SRepsThenSets
+    {
+        [Required]
+        public string UniqueId { get; set; }
+        [Required]
+        public int StartingReps { get; set; }
+        [Required]
+        public int StartingSets { get; set; }
+        [Required]
+        public int RepIncreasePerSet { get; set; }
+    }
+}
